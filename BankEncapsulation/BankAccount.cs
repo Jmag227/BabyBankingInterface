5abd63bff8c02fd81e24b5cc74e87117cec9393f
Jarom Magalei <jarom.pro.magalei@gmail.com>
2021-12-14 14:12:26 -0700

    First commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        public BankAccount()
+        {
+
+        }
+        private double balance;
+
+        public BankAccount(double _balance)
+        {
+            balance = _balance;
+        }
+
+
+
+        public void Deposit(double x)
+        {
+            balance = balance + x;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           Deposit(Greet());
+
+
+
+
+
+        }
+
+        public static string Greet()
+        {
+            Console.WriteLine("Hello, Welcome to Magalei Banking Mobile!");
+            Console.WriteLine();
+            Console.WriteLine("Please enter your name.");
+            Console.WriteLine();
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}, thank you for choosing MBM!");
+            Console.WriteLine();
+            return name;
+        }
+
+        public static void Deposit(string name)
+        {
+            Console.WriteLine();
+            bool didParse;
+            double y;
+            Console.WriteLine("Please enter the amount you would like to donate.. er, deposite!");
+
+            do
+            {
+                didParse = double.TryParse(Console.ReadLine(), out double x);
+                y = x;
+                if(didParse == false)
+                {
+                    Console.WriteLine("That is not a valid number, try again");
+                } 
+                   
+            } while (!didParse);
+
+            var depo = new BankAccount();
+            depo.Deposit(y);
+            Console.WriteLine();
+
+            var num = depo.GetBalance();
+            Console.WriteLine($"Your new Balance is {num}");
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();                         
+        }
+
+        public static void LeaveOrNO()
+        {
+            Console.WriteLine("Would you like or make another deposite?");
+        }
+
+
+
+    }
+}
